db30f7a3a21dc3e23c5e733da38acd629d7bbb87
ThienBNguyen <thien.nguyen30191@gmail.com>
2022-05-10 18:32:30 -0500

    add client to git

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import Appbar from './components/Appbar';
+import './App.css';
+import Student from './components/Student';
+
+function App() {
+	return (
+		<div className="App">
+			<Appbar />
+			<Student />
+		</div>
+	);
+}
+
+export default App;
